7f7cc318da440fbfeb03dbe5ef1770ba96eb3fa7
unknown <akpjunio4real@gmail.com>
2020-03-23 19:46:54 +0100

    add odd and even logic

---
+++ odd-even.js
@@ -0,0 +1,12 @@
+let myName = 'Akpan Abraham';
+var myCourses = ["HTML", "CSS", "JavaScript"];
+
+console.log(myName);
+
+console.log(myCourses);
+
+for (i = 1; i <= 200; i++) {
+    if (i % 2 !== 0) {
+        console.log(i + " is odd");
+    }
+}
